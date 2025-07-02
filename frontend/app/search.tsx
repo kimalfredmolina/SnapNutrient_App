64234520bce3629d4255f9d4f6bd3fb0de5201d1
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-07-02 16:46:12 +0800

    initial commit

---
+++ frontend/app/search.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Search() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text className="text-2xl font-bold">Search ahahha Screen</Text>
+      <Text className="text-gray-500 mt-2">Manage your search here.</Text>
+    </View>
+  );
+}
