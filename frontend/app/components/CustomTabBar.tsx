388944234daf19440c4bf2f60e054074e8eee334
Jp-22-00540 <nel.penaflor12@gmail.com>
2025-08-13 19:24:33 +0800

    add the tabIndex/macro to hide in navigation bar

--- frontend/app/components/CustomTabBar.tsx
+++ frontend/app/components/CustomTabBar.tsx
@@ -18,7 +18,7 @@ export default function CustomTabBar({ state, descriptors, navigation }: BottomT
   const currentRouteName = currentRoute?.name
 
   // Hide bottom navigation on these pages
-  const hiddenRoutes = ["tabSetting/settings", "tabSetting/about", "tabSetting/privacy", "tabSetting/contact", "tabSetting/help", "tabSetting/report" , "tabHistory/history-detail"]
+  const hiddenRoutes = ["tabSetting/settings", "tabSetting/about", "tabSetting/privacy", "tabSetting/contact", "tabSetting/help", "tabSetting/report" , "tabHistory/history-detail", "tabIndex/MacroCalculatorModal"]
 
   // Hide the entire tab bar when on scan page
   if (currentRouteName === "scan") {
