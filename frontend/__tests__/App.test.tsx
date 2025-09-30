9b78f08663c22410536b5be24bdda564685d8fa3
Kim Alfred A. Molina <119164038+kimalfred@users.noreply.github.com>
2025-09-30 15:43:33 +0800

    Added More unit testing

--- frontend/__tests__/App.test.tsx
+++ frontend/__tests__/App.test.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
 import App from "../app/(auth)/signin";
+import AccountPage from "../app/pages/account";
+import HistoryPage from "../app/pages/history";
+import IndexPage from "../app/pages/index";
+import ScanPage from "../app/pages/scan";
+import StatisticsPage from "../app/pages/statistics";
 import { ThemeProvider } from "../contexts/ThemeContext";
 
 // Add AsyncStorage mock
@@ -19,6 +24,53 @@ describe("App", () => {
       </ThemeProvider>
     );
     // Use toJSON() to check if the rendered output exists
+    expect(toJSON()).toBeTruthy();
+  });
+});
+
+describe("Main Pages", () => {
+  it("renders Account page correctly", () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <AccountPage />
+      </ThemeProvider>
+    );
+    expect(toJSON()).toBeTruthy();
+  });
+
+  it("renders History page correctly", () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <HistoryPage />
+      </ThemeProvider>
+    );
+    expect(toJSON()).toBeTruthy();
+  });
+
+  it("renders Index page correctly", () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <IndexPage />
+      </ThemeProvider>
+    );
+    expect(toJSON()).toBeTruthy();
+  });
+
+  it("renders Scan page correctly", () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <ScanPage />
+      </ThemeProvider>
+    );
+    expect(toJSON()).toBeTruthy();
+  });
+
+  it("renders Statistics page correctly", () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <StatisticsPage />
+      </ThemeProvider>
+    );
     expect(toJSON()).toBeTruthy();
   });
 });
